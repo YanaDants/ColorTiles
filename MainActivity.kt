b60b2d844c3705bc4299a9c6e476c6e25df2c1dc
YanaDants <115608585+YanaDants@users.noreply.github.com>
2023-10-28 21:47:07 +0800

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.col
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(NewView(this))
+    }
+}
